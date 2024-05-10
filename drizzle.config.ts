99e1981530c6a9dfe51409409b7d62535d20339f
Pedro Santiago <pedrossantiago1@gmail.com>
2024-05-10 14:10:24 -0300

    chore: Update drizzle configuration to use SQLite dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from 'drizzle-kit';
 export default {
+	dialect: 'sqlite',
 	schema: './src/lib/db/schema.ts',
 	out: './drizzle',
-	driver: 'better-sqlite',
 	dbCredentials: {
 		url: './sqlite.db'
 	}
