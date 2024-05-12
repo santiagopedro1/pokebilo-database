50d08336b6a7aecbc09b9fc6ae48c0ff0b248229
Pedro Santiago <pedrossantiago1@gmail.com>
2024-05-12 01:08:01 -0300

    refactor: Remove unused code for Pokemon page

--- src/routes/[pokemon]/+page.svelte
+++ src/routes/[pokemon]/+page.svelte
@@ -87,25 +87,3 @@
 		/>
 	</PokemonSectionCard>
 </div>
-<!-- #c87d37
-#7b8c42
-#b13d3d
-#5e8a94
-#a4617d
-#94631d
-#5f7e6a
-#9a6e27
-#536f85
-#9d4e9d -->
-<div class="flex gap-4">
-	<div class="size-80 bg-[#c87d37]"></div>
-	<div class="size-80 bg-[#7b8c42]"></div>
-	<div class="size-80 bg-[#b13d3d]"></div>
-	<div class="size-80 bg-[#5e8a94]"></div>
-	<div class="size-80 bg-[#a4617d]"></div>
-	<div class="size-80 bg-[#94631d]"></div>
-	<div class="size-80 bg-[#5f7e6a]"></div>
-	<div class="size-80 bg-[#9a6e27]"></div>
-	<div class="size-80 bg-[#536f85]"></div>
-	<div class="size-80 bg-[#9d4e9d]"></div>
-</div>
